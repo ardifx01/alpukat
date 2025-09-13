533ad78f2d6b744974928868e777bf885aa8102f
Amelia <122196414+Mellias@users.noreply.github.com>
2025-09-13 23:46:59 +0700

    Menghapus vite pada halaman auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Daftar | ALPUKAT</title>
-  @vite(['resources/css/app.css','resources/js/app.js'])
   <link rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
       integrity="sha384-..." crossorigin="anonymous">
