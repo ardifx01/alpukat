dddb23d9383509896e3783774a32f980bfc2be34
Amelia <122196414+Mellias@users.noreply.github.com>
2025-09-13 23:34:16 +0700

    Hapus import di app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,3 @@
-import './bootstrap';
-import './alpukat.js';
-import './bootstrap.js';
-import './custom.js';
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
